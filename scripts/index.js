1e3107ae8d823a6bb663e5ebc4c3a916051b7564
Kirill Dneprovskiy <dneprovskiykirya@gmail.com>
2024-02-01 17:31:59 +0800

    Ready to review

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const placesList = document.querySelector(".places__list");
+
+function addCard(item) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__image").src = item.link;
+  cardElement.querySelector(".card__title").textContent = item.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", removeCard);
+
+  function removeCard() {
+    const cardItem = cardElement
+      .querySelector(".card__delete-button")
+      .closest(".card");
+    cardItem.remove();
+  };
+
+  placesList.append(cardElement);
+}
+
+initialCards.forEach(addCard);
 
 // @todo: DOM узлы
 
