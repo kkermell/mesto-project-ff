8a58e1b1281c23a146f7358c8b77bbd2367a0373
Kirill Dneprovskiy <dneprovskiykirya@gmail.com>
2024-02-03 13:29:31 +0800

    review 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,28 @@
 const cardTemplate = document.querySelector("#card-template").content;
 const cardsContainer = document.querySelector(".places__list");
 
-function createCard(cardData) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
-  const deleteButton = cardElement.querySelector(".card__delete-button");
+function createCard(cardData, deleteCard) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  const deleteButton = card.querySelector(".card__delete-button");
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
 
-  cardElement.querySelector(".card__image").src = cardData.link;
-  cardElement.querySelector(".card__image").alt = cardData.name;
-  cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
   deleteButton.addEventListener("click", () => {
-    deleteButton.closest(".card").remove();
+    deleteCard(card);
   });
 
-  return cardElement;
+  return card;
+}
+
+function deleteCard(card) {
+  card.remove();
 }
 
 initialCards.forEach((cardData) => {
-  const card = createCard(cardData);
+  const card = createCard(cardData, deleteCard);
   cardsContainer.append(card);
 });
